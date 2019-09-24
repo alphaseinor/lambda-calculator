f12b3d0a34fb3965f997c9b38dbd1e068fbcc4a5
Brian Hague <alphaseinor@gmail.com>
2019-09-23 22:32:57 -0500

    attempting the calculator numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import {useState, React} from "react";
+import {numbers} from "../../../data.js";
+import {NumberButton} from "./NumberButton.js";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -7,13 +9,20 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numbers, setNumberState] = useState(numbers);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {
+        numbers.map((number, index) => {
+          <NumberButton key={number} />
+        })
+      }
     </div>
   );
 };
