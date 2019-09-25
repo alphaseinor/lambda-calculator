4094b838322afdd7154340913d9d7cd0cc292cae
alphaseinor <alphaseinor@gmail.com>
2019-09-24 19:00:26 -0500

    added number buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React from 'react';
 
-export const NumberButton = (props) => {
+export const NumberButton = props => {
   return (
-    <button 
-      className="number_button" 
-    >{props.key}
+    <button className="number_button" key={props.id}>
+      {props.number}
+      {console.log(props)}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
-
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,9 +1,9 @@
-import {useState, React} from "react";
-import {numbers} from "../../../data.js";
-import {NumberButton} from "./NumberButton.js";
+import React, { useState } from 'react';
+import { numbers } from '../../../data.js';
+import { NumberButton } from './NumberButton.js';
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
@@ -11,18 +11,16 @@ import { numbers } from '../../../data'
 
 export const Numbers = () => {
   // STEP 2 - add the imported data to state
-  const [numbers, setNumberState] = useState(numbers);
+  const [nums, setNums] = useState(numbers);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-      {
-        numbers.map((number, index) => {
-          <NumberButton key={number} />
-        })
-      }
+      {nums.map((num, index) => {
+        return <NumberButton key={index} number={num} state={useState} />;
+      })}
     </div>
   );
 };
