0cea647a744f8e0038a98226d887113ab23c6086
Brian Hague <alphaseinor@gmail.com>
2019-09-24 20:27:39 -0500

    added special buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const SpecialButton = () => {
+export const SpecialButton = (props) => {
+  console.log(props)
   return (
-    <>
+    <button 
+      className="special_button"
+      key={props.id}
+      value={props.special}
+    >
+    {props.special}  
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
+
   );
 };
